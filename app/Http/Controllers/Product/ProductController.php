b47fdd0c9a3b6080f9e921bd61302feb55f0376f
Dayfob <semembaevalihan19@gmail.com>
2022-04-28 20:50:37 +0600

    added calculated parameter "liked" for response products

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\Product;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product\Product;
+use App\Models\User\Wishlist;
+use App\Models\User\WishlistProduct;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use stdClass;
 
 class ProductController extends Controller
 {
@@ -13,70 +17,169 @@ class ProductController extends Controller
         $products = Product::all();
 
         foreach ($products as $product){
-            $product->brand_id = $product->brand;
-            $product->category_id = $product->category;
-            $product->subcategory_id = $product->subcategory;
-            $product->image_url = asset('storage/' . $product->image_url);
+            $productsResponse = new stdClass();
+            $productsResponse->id = $product->id;
+            $productsResponse->name = $product->name;
+            $productsResponse->slug = $product->slug;
+            $productsResponse->image_url = asset('storage/' . $product->image_url);
+            $productsResponse->description = $product->description;
+            $productsResponse->brand_id = $product->brand;
+            $productsResponse->category_id = $product->category;
+            $productsResponse->subcategory_id = $product->subcategory;
+            $productsResponse->amount_left = $product->amount_left;
+            $productsResponse->price = $product->price;
+            $productsResponse->attributes = $product->attributes;
+            $productsResponse->liked = false;
+
+            if($user = Auth::user()){
+                $wishlist = Wishlist::whereUserId($user->id)->first();
+                $wishlistProducts = WishlistProduct::whereWishlistId($wishlist->id)->get();
+                foreach ($wishlistProducts as $wishlistProduct){
+                    if ($wishlistProduct->item_id === $product->id){
+                        $productsResponse->liked = true;
+                    }
+                }
+            }
+            $responseArray[] = $productsResponse;
         }
 
-        return response()->json($products);
+        return response()->json($responseArray);
     }
 
     public function getCategoryProducts(Request $request)
     {
-        $categoryId = $request->get('category_id');
+        $categoryId = $request->input('category_id');
         $products = Product::whereCategoryId($categoryId)->get();
 
         foreach ($products as $product){
-            $product->brand_id = $product->brand;
-            $product->category_id = $product->category;
-            $product->subcategory_id = $product->subcategory;
-            $product->image_url = asset('storage/' . $product->image_url);
+            $productsResponse = new stdClass();
+            $productsResponse->id = $product->id;
+            $productsResponse->name = $product->name;
+            $productsResponse->slug = $product->slug;
+            $productsResponse->image_url = asset('storage/' . $product->image_url);
+            $productsResponse->description = $product->description;
+            $productsResponse->brand_id = $product->brand;
+            $productsResponse->category_id = $product->category;
+            $productsResponse->subcategory_id = $product->subcategory;
+            $productsResponse->amount_left = $product->amount_left;
+            $productsResponse->price = $product->price;
+            $productsResponse->attributes = $product->attributes;
+            $productsResponse->liked = false;
+
+            if($user = Auth::user()){
+                $wishlist = Wishlist::whereUserId($user->id)->first();
+                $wishlistProducts = WishlistProduct::whereWishlistId($wishlist->id)->get();
+                foreach ($wishlistProducts as $wishlistProduct){
+                    if ($wishlistProduct->item_id === $product->id){
+                        $productsResponse->liked = true;
+                    }
+                }
+            }
+            $responseArray[] = $productsResponse;
         }
 
-        return response()->json($products);
+        return response()->json($responseArray);
     }
 
     public function getSubcategoryProducts(Request $request)
     {
-        $subcategoryId = $request->get('subcategory_id');
+        $subcategoryId = $request->input('subcategory_id');
         $products = Product::whereSubcategoryId($subcategoryId)->get();
 
         foreach ($products as $product){
-            $product->brand_id = $product->brand;
-            $product->category_id = $product->category;
-            $product->subcategory_id = $product->subcategory;
-            $product->image_url = asset('storage/' . $product->image_url);
+            $productsResponse = new stdClass();
+            $productsResponse->id = $product->id;
+            $productsResponse->name = $product->name;
+            $productsResponse->slug = $product->slug;
+            $productsResponse->image_url = asset('storage/' . $product->image_url);
+            $productsResponse->description = $product->description;
+            $productsResponse->brand_id = $product->brand;
+            $productsResponse->category_id = $product->category;
+            $productsResponse->subcategory_id = $product->subcategory;
+            $productsResponse->amount_left = $product->amount_left;
+            $productsResponse->price = $product->price;
+            $productsResponse->attributes = $product->attributes;
+            $productsResponse->liked = false;
+
+            if($user = Auth::user()){
+                $wishlist = Wishlist::whereUserId($user->id)->first();
+                $wishlistProducts = WishlistProduct::whereWishlistId($wishlist->id)->get();
+                foreach ($wishlistProducts as $wishlistProduct){
+                    if ($wishlistProduct->item_id === $product->id){
+                        $productsResponse->liked = true;
+                    }
+                }
+            }
+            $responseArray[] = $productsResponse;
         }
 
-        return response()->json($products);
+        return response()->json($responseArray);
     }
 
     public function getBrandProducts(Request $request)
     {
-        $brandId = $request->get('brand_id');
+        $brandId = $request->input('brand_id');
         $products = Product::whereBrandId($brandId)->get();
 
         foreach ($products as $product){
-            $product->brand_id = $product->brand;
-            $product->category_id = $product->category;
-            $product->subcategory_id = $product->subcategory;
-            $product->image_url = asset('storage/' . $product->image_url);
+            $productsResponse = new stdClass();
+            $productsResponse->id = $product->id;
+            $productsResponse->name = $product->name;
+            $productsResponse->slug = $product->slug;
+            $productsResponse->image_url = asset('storage/' . $product->image_url);
+            $productsResponse->description = $product->description;
+            $productsResponse->brand_id = $product->brand;
+            $productsResponse->category_id = $product->category;
+            $productsResponse->subcategory_id = $product->subcategory;
+            $productsResponse->amount_left = $product->amount_left;
+            $productsResponse->price = $product->price;
+            $productsResponse->attributes = $product->attributes;
+            $productsResponse->liked = false;
+
+            if($user = Auth::user()){
+                $wishlist = Wishlist::whereUserId($user->id)->first();
+                $wishlistProducts = WishlistProduct::whereWishlistId($wishlist->id)->get();
+                foreach ($wishlistProducts as $wishlistProduct){
+                    if ($wishlistProduct->item_id === $product->id){
+                        $productsResponse->liked = true;
+                    }
+                }
+            }
+            $responseArray[] = $productsResponse;
         }
 
-        return response()->json($products);
+        return response()->json($responseArray);
     }
 
     public function getProduct(Request $request)
     {
-        $productId = $request->get('product_id');
+        $productId = $request->input('product_id');
         $product = Product::whereId($productId)->first();
 
-        $product->brand_id = $product->brand;
-        $product->category_id = $product->category;
-        $product->subcategory_id = $product->subcategory;
-        $product->image_url = asset('storage/' . $product->image_url);
+        $productsResponse = new stdClass();
+        $productsResponse->id = $product->id;
+        $productsResponse->name = $product->name;
+        $productsResponse->slug = $product->slug;
+        $productsResponse->image_url = asset('storage/' . $product->image_url);
+        $productsResponse->description = $product->description;
+        $productsResponse->brand_id = $product->brand;
+        $productsResponse->category_id = $product->category;
+        $productsResponse->subcategory_id = $product->subcategory;
+        $productsResponse->amount_left = $product->amount_left;
+        $productsResponse->price = $product->price;
+        $productsResponse->attributes = $product->attributes;
+        $productsResponse->liked = false;
 
-        return response()->json($product);
+        if($user = Auth::user()){
+            $wishlist = Wishlist::whereUserId($user->id)->first();
+            $wishlistProducts = WishlistProduct::whereWishlistId($wishlist->id)->get();
+            foreach ($wishlistProducts as $wishlistProduct){
+                if ($wishlistProduct->item_id === $product->id){
+                    $productsResponse->liked = true;
+                }
+            }
+        }
+
+        return response()->json($productsResponse);
     }
 }
