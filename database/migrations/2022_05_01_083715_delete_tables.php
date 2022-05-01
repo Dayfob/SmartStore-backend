51281fb8a43f6c3ed3f7f023f4f6f2ac8204a1c7
Nikita Kirichevskiy <nikitakirichevskiy@gmail.com>
2022-05-01 14:44:20 +0600

    Edit DB

---
+++ database/migrations/2022_05_01_083715_delete_tables.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DeleteTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('admin_roles');
+        Schema::dropIfExists('admin_roles_employers');
+        Schema::dropIfExists('admin_roles_permissions');
+        Schema::table('product_category_subcategories', function(Blueprint $table) {
+            $table->string('image_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
