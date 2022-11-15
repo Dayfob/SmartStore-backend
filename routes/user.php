82dbddf696bea9184951f78b8c9d1dce8750f466
Dayfob <semembaevalihan19@gmail.com>
2022-11-15 10:59:32 +0600

    Добавили http-тесты: BrandController                      CategoryController                      ProductController                      SubcategoryController                      UserController                      WishlistController

--- routes/user.php
+++ routes/user.php
@@ -6,14 +6,10 @@ use App\Http\Controllers\User\UserController;
 use App\Http\Controllers\User\WishlistController;
 use Illuminate\Support\Facades\Route;
 
-//use App\Http\Controllers\Warehouse\LeftoversController;
-//use App\Http\Controllers\Warehouse\PriceController;
-
 Route::group(['prefix' => 'user'], function () {
     Route::post('login', [UserController::class, 'login']);
     Route::post('register',[UserController::class, 'register']);
 
-//    Route::middleware(['auth:api'])-> group(function() {
     Route::middleware(['auth:sanctum'])-> group(function() {
         Route::get('me', [UserController::class, 'getUser']);
         Route::post('logout', [UserController::class, 'logout']);
@@ -40,18 +36,5 @@ Route::group(['prefix' => 'user'], function () {
             Route::post('delete_order', [OrderController::class, 'deleteOrder']);
             Route::post('invoices', [OrderController::class, 'sendInvoice']);
         });
-
     });
-
-//    Route::get('search', [LeftoversController::class, 'searchInWarehouses']);
-//    Route::get('product', [LeftoversController::class, 'getProductByGuid']);
-//    Route::get('crosses', [LeftoversController::class, 'getLaximoCrosses']);
-//
-//    Route::group(['middleware' => ['auth:web', 'novaAdmin'], 'prefix' => 'price'], function () {
-//        Route::get('correlations', [PriceController::class, 'getCorrelations']);
-//        Route::get('correlations/{id}', [PriceController::class, 'getCorrelationById']);
-//        Route::delete('correlations/{id}', [PriceController::class, 'deleteCorrelationById']);
-//        Route::post('createRule', [PriceController::class, 'createRule']);
-//        Route::post('updateRule', [PriceController::class, 'updateRule']);
-//    });
 });
